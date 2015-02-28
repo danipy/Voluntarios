1463d86d358467e8667703d6234dbbeb485e15e1
Dani <ronaldanny@hotmail.com>
2015-02-27 21:57:06 -0600

    2 querys nuevos
    - query para traer los voluntarios de una ong
    
    - query para traer los voluntarios inscritos en un evento

--- src/main/java/mx/voluntarios/repository/VolunteerRepository.java
+++ src/main/java/mx/voluntarios/repository/VolunteerRepository.java
@@ -1,10 +1,20 @@
 package mx.voluntarios.repository;
 
+import java.util.List;
+
 import mx.voluntarios.domain.Volunteer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
+
+	@Query("SELECT v FROM Volunteer v JOIN v.ongs o WHERE o.id = :ongId")
+	List<Volunteer> findByOng(@Param("ongId") Long ongId);
+
+	@Query("SELECT v FROM Volunteer v JOIN v.events e WHERE e.id = :eventId")
+	List<Volunteer> findByEvent(@Param("eventId") Long eventId);
 
 }
