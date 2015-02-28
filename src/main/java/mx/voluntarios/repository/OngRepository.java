ffadc8706ec5faa4f888fabfa4414cf90e9d6082
Dani <ronaldanny@hotmail.com>
2015-02-27 21:55:54 -0600

    query para traer los voluntarios de una ong

--- src/main/java/mx/voluntarios/repository/OngRepository.java
+++ src/main/java/mx/voluntarios/repository/OngRepository.java
@@ -1,9 +1,16 @@
 package mx.voluntarios.repository;
 
+import java.util.List;
+
 import mx.voluntarios.domain.Ong;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OngRepository extends JpaRepository<Ong, Long> {
+
+	@Query("SELECT o FROM Ong o JOIN o.volunteers v WHERE v.id = :ongId")
+	List<Ong> findByVolunteer(@Param("ongId") Long ongId);
 
 }
