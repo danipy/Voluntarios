1cddceaafa5e2e48feb5ec87f9b9c401b2b9b178
Dani <ronaldanny@hotmail.com>
2015-02-27 21:55:35 -0600

    2 metodos nuevos
    - se agrega metodo para traer los eventos de una organizacion
    
    - se agrega query para traer los eventos registrados por una ong

--- src/main/java/mx/voluntarios/repository/EventRepository.java
+++ src/main/java/mx/voluntarios/repository/EventRepository.java
@@ -1,13 +1,23 @@
 package mx.voluntarios.repository;
 
 import java.util.Date;
+import java.util.List;
 
 import mx.voluntarios.domain.Event;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+	
 	Page<Event> findByDateAfter(Date date, Pageable pageable);
+	
+	List<Event> findByOngId(Long id);
+
+	@Query("SELECT e FROM Event e JOIN e.volunteers v WHERE v.id = :eventId")
+	List<Event> findByVolunteer(@Param("eventId") Long eventId);
+
 }
